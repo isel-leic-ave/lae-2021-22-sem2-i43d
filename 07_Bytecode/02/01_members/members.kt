2ce2e1a978c2548adf41cda347d93e66ca9ee6e4
João Trindade <joao.trindade@isel.pt>
2022-04-28 09:58:56 +0100

    Bytecode (instance and static members; invoke instructions)

---
+++ 07_Bytecode/02/01_members/members.kt
@@ -0,0 +1,22 @@
+
+class MembersDemo(val msg : String) {
+	fun showMsg() {
+		println(msg)
+	}
+}
+
+val msg = "Hello, world!"
+
+fun showMsg() {
+	println(msg)
+}
+
+fun main() {
+	showMsg()
+	
+	val demo1 = MembersDemo("Hello, LAE!")
+	val demo2 = MembersDemo("Hello, LEIC43D!")
+	
+	demo1.showMsg()
+	demo2.showMsg()
+}
