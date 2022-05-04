177e64fc68ffedbb7f2f55b37ab9fca6c79809e7
João Trindade <joao.trindade@isel.pt>
2022-05-04 10:55:06 +0100

    JMH (List vs Set)

--- 08_JMH/jmh/jmh01/src/jmh/java/pt/isel/leic/lae/BasicBenchmark.java
+++ 08_JMH/jmh/jmh01/src/jmh/java/pt/isel/leic/lae/BasicBenchmark.java
@@ -9,14 +9,29 @@ import java.util.function.*;
 
 @BenchmarkMode(Mode.AverageTime)
 @Fork(value = 1, warmups = 1)
-@Measurement(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
 @Warmup(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class BasicBenchmark {
 
-	@Benchmark
+	private static final List<String> listOfWords = Arrays.asList("Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu");
+
+	private static final Set<String> setOfWords = new HashSet<String>(listOfWords);
+
+
+	//@Benchmark
 	public void emptyMethod() {
 		
+	}
+
+	@Benchmark
+	public boolean findInList() {
+		return listOfWords.contains("Mike");
+	}
+
+	@Benchmark
+	public boolean findInSet() {
+		return setOfWords.contains("Mike");
 	}
 
 }
